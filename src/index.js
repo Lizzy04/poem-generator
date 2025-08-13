15e044fc4d6d5b462c4742cc3171923b9961b7fd
Lizzy04 <109428514+Lizzy04@users.noreply.github.com>
2025-08-13 15:51:23 +0200

    Added basic Javascript form handling and added Typewriter plugin

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function generateForm(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemElement = document.querySelector("#poem");
+poemElement.innerHTML = "La tombe dit à la rose";
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generateForm);

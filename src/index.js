75034a434ff96522523a91baaa0a57d5fa7d3396
Lizzy04 <109428514+Lizzy04@users.noreply.github.com>
2025-08-15 19:53:50 +0200

    Removed the triple backticks and html identifier from poem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 function displayPoem(response) {
+  let poem = response.data.answer;
+
+  //Remove triple backticks
+  poem = poem.replace(/```(?:html)?/gi, "").trim();
+  poem = poem.replace(/```/g, "").trim();
+
   new Typewriter("#poem", {
-    strings: response.data.answer,
+    strings: poem,
     autoStart: true,
     delay: 1,
     cursor: "",
@@ -13,7 +19,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "eb44271etfeaa70ae19b3f316ao64530";
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow the user instructions and separate each line with a <br>. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
